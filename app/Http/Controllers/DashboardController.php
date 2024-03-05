fe4fc86ef565f24ade9bb827d89ca9bf654d306b
NabilahSri <billb4721@gmail.com>
2024-03-05 18:51:33 +0700

    final project

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pengajuan;
+use App\Models\TransaksiS;
+use App\Models\TransaksiT;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index(){
+        $data['jumlah_bayar_pinjaman'] = TransaksiT::where('id_kategori', 3)->where('id_user',  auth()->user()->id)->sum('jumlah');
+        $data['jumlah_bayar_bagihasil'] = TransaksiT::where('id_kategori', 4)->where('id_user',  auth()->user()->id)->sum('jumlah');
+
+        $data['pinjaman'] = Pengajuan::where('id_user',  auth()->user()->id)->first();
+        $data['sisa_pinjaman'] = $data['pinjaman']->nominal_pinjaman - $data['jumlah_bayar_pinjaman'];
+
+        $data['bagihasil'] = Pengajuan::where('id_user',  auth()->user()->id)->first();
+        $data['sisa_bagihasil'] = $data['bagihasil']->nominal_bagihasil - $data['jumlah_bayar_bagihasil'];
+
+        $data['jumlah_simpanan'] = User::where('id', auth()->user()->id)->first();
+        $data['total_iuran_wajib'] = TransaksiS::where('id_kategori', 2)->where('id_user',  auth()->user()->id)->sum('jumlah');
+
+        $data['total_admin'] = User::where('role', 'admin')->sum('role');
+        $data['total_anggota'] = User::where('role', 'anggota')->sum('role');
+
+        $data['total_semua_simpanan'] = User::sum('iuran_wajib');
+        $data['total_semua_tagihan'] = Pengajuan::sum('nominal_pinjaman') + Pengajuan::sum('nominal_bagihasil');
+
+        $tanggal = Carbon::now();
+        $data['simpanan_masuk'] = TransaksiS::where('tanggal', $tanggal)->with('users')->get();
+
+
+        return view('pages.dashboard', $data);
+    }
+
+
+}
